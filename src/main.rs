305626b7670b47b37a44555ea888b740ccf7c174
Vitor <vitor.aquino70@gmail.com>
2022-08-17 19:16:31 -0300

    add get_all_items

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
         .mount("/", routes![get_neighborhood_wrapper, create_neighborhood])
         .mount("/", routes![get_address_wrapper, create_address])
+        .mount("/", routes![get_item_wrapper, create_item, get_all_items])
         .mount("/", routes![get_item_wrapper, create_item])
         .mount("/", routes![get_motoboy_wrapper, create_motoboy])
         .mount("/", routes![get_order_details_wrapper, create_order_details])
