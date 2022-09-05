68d44d6966c10934c587f79d4073656ba5aa2ade
Vitor <vitor.aquino70@gmail.com>
2022-09-05 15:35:04 -0300

    add features to address.rs

--- src/libs/motoboy.rs
+++ src/libs/motoboy.rs
@@ -2,9 +2,12 @@ use super::super::schema::motoboy;
 use diesel::prelude::*;
 use rocket::form::{Form, FromForm};
 use crate::DATABASE_URL;
+use serde::Serialize;
+use rocket::serde::json::Json;
+use rocket::response::status;
 
 
-#[derive(Debug, Queryable)]
+#[derive(Debug, Queryable, Serialize)]
 pub struct Motoboy {
     pub id: i32,
     pub name: String,
@@ -14,7 +17,7 @@ pub struct Motoboy {
 }
 
 
-#[derive(Debug, Insertable, FromForm)]
+#[derive(Debug, AsChangeset, Insertable, FromForm)]
 #[table_name="motoboy"]
 pub struct NewMotoboy {
     pub name: String,
@@ -24,19 +27,19 @@ pub struct NewMotoboy {
 }
 
 #[get("/motoboy/<motoboy_id>")]
-pub fn get_motoboy_wrapper(motoboy_id: i32) -> String {
+pub fn get_motoboy(motoboy_id: i32) -> Result<Json<Motoboy>, status::NotFound<String>> {
     let conn = PgConnection::establish(DATABASE_URL)
         .unwrap_or_else(|_| panic!("Error connecting to {}", DATABASE_URL));
 
-    let motoboy = get_motoboy(&conn, motoboy_id);
+    let motoboy = _get_motoboy(&conn, motoboy_id);
 
     match motoboy {
-        Some(motoboy) => format!("{:?}", motoboy),
-        None => "Motoboy not found".to_string(),
+        Some(motoboy) => Ok(Json(motoboy)),
+        None => Err(status::NotFound("Motoboy not found".to_string())),
     }
 }
 
-pub fn get_motoboy(conn: &PgConnection, motoboy_id: i32) -> Option<Motoboy> {
+pub fn _get_motoboy(conn: &PgConnection, motoboy_id: i32) -> Option<Motoboy> {
     motoboy::table
         .find(motoboy_id)
         .first::<Motoboy>(conn)
@@ -45,14 +48,73 @@ pub fn get_motoboy(conn: &PgConnection, motoboy_id: i32) -> Option<Motoboy> {
 }
 
 #[post("/motoboy", data = "<motoboy>")]
-pub fn create_motoboy(motoboy: Form<NewMotoboy>) -> String {
+pub fn create_motoboy(motoboy: Form<NewMotoboy>) -> Result<Json<Motoboy>, status::BadRequest<String>> {
     let conn = PgConnection::establish(DATABASE_URL)
         .unwrap_or_else(|_| panic!("Error connecting to {}", DATABASE_URL));
 
-    let motoboy = diesel::insert_into(motoboy::table)
-        .values(motoboy.into_inner())
-        .get_result::<Motoboy>(&conn)
-        .expect("Error creating motoboy");
+    let motoboy = _create_motoboy(&conn, motoboy.into_inner());
 
-    format!("{:?}", motoboy)
+    match motoboy {
+        Ok(motoboy) => Ok(Json(motoboy)),
+        Err(_) => Err(status::BadRequest(Some("Error creating motoboy".to_string()))),
+    }
+}
+
+fn _create_motoboy(conn: &PgConnection, motoboy: NewMotoboy) -> QueryResult<Motoboy> {
+    diesel::insert_into(motoboy::table)
+        .values(motoboy)
+        .get_result::<Motoboy>(conn)
+}
+
+#[put("/motoboy/<motoboy_id>", data = "<motoboy>")]
+pub fn update_motoboy(motoboy_id: i32, motoboy: Form<NewMotoboy>) -> Result<Json<Motoboy>, status::BadRequest<String>> {
+    let conn = PgConnection::establish(DATABASE_URL)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", DATABASE_URL));
+
+    let motoboy = _update_motoboy(&conn, motoboy_id, motoboy.into_inner());
+
+    match motoboy {
+        Ok(motoboy) => Ok(Json(motoboy)),
+        Err(_) => Err(status::BadRequest(Some("Error updating motoboy".to_string()))),
+    }
+}
+
+fn _update_motoboy(conn: &PgConnection, motoboy_id: i32, motoboy: NewMotoboy) -> QueryResult<Motoboy> {
+    diesel::update(motoboy::table.find(motoboy_id))
+        .set(motoboy)
+        .get_result::<Motoboy>(conn)
+}
+
+#[delete("/motoboy/<motoboy_id>")]
+pub fn delete_motoboy(motoboy_id: i32) -> Result<Json<Motoboy>, status::BadRequest<String>> {
+    let conn = PgConnection::establish(DATABASE_URL)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", DATABASE_URL));
+
+    let motoboy = _delete_motoboy(&conn, motoboy_id);
+
+    match motoboy {
+        Ok(motoboy) => Ok(Json(motoboy)),
+        Err(_) => Err(status::BadRequest(Some("Error deleting motoboy".to_string()))),
+    }
+}
+
+fn _delete_motoboy(conn: &PgConnection, motoboy_id: i32) -> QueryResult<Motoboy> {
+    diesel::delete(motoboy::table.find(motoboy_id))
+        .get_result::<Motoboy>(conn)
+}
+
+#[get("/motoboy")]
+pub fn get_motoboys() -> Json<Vec<Motoboy>> {
+    let conn = PgConnection::establish(DATABASE_URL)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", DATABASE_URL));
+
+    let motoboys = _get_motoboys(&conn);
+
+    Json(motoboys)
+}
+
+fn _get_motoboys(conn: &PgConnection) -> Vec<Motoboy> {
+    motoboy::table
+        .load::<Motoboy>(conn)
+        .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> Result<(), rocket::Error> {
         .mount("/", routes![get_neighborhood_wrapper, create_neighborhood, get_neighborhoods, update_neighborhood, delete_neighborhood])
         .mount("/", routes![get_address, create_address, get_addresses, update_address, delete_address])
         .mount("/", routes![get_item_wrapper, create_item, update_item, delete_item, get_all_items])
-        .mount("/", routes![get_motoboy_wrapper, create_motoboy])
+        .mount("/", routes![get_motoboy, create_motoboy, update_motoboy, delete_motoboy, get_motoboys])
         .mount("/", routes![get_order_details_wrapper, create_order_details])
         .mount("/", routes![get_customer_addresses_wrapper, create_customer_address])
         .mount("/", routes![get_customer_wrapper, create_customer])
